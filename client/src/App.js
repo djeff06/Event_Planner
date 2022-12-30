7fab2ea404742894508c8cd4efeac19a46c20d9b
AIT HELLAL Djaaffer <djaaffer.aithellal@gmail.com>
2022-12-30 02:04:42 +0100

    implementing sidebar user and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar"
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/login-signup/Login";
 import Signup from "./pages/login-signup/Signup";
 import { Auth } from "./contexts/Auth";
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { MyProSidebarProvider } from "./components/sidebar/sidebarContext";
+import Topbar from "./components/Topbar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Dashboard from "./pages/Dashbord";
 
 function App() {
   const { user } = useContext(Auth);
@@ -14,22 +19,49 @@ function App() {
     document.body.className = theme1;
   }, [theme1]);
 
+  const [theme, colorMode] = useMode();
+
   return (
     <BrowserRouter>
-      <div className={`${theme1}`}>
-        {!user && <Navbar theme1={theme1} setTheme1={setTheme1} />}{" "}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/login"
-            element={!user ? <Login /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/signup"
-            element={!user ? <Signup /> : <Navigate to="/" />}
-          />
-        </Routes>
-      </div>
+      <>
+        <div style={{ height: "100%", width: "100%" }}>
+          {!user && <Navbar theme1={theme1} setTheme1={setTheme1} />}
+          <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <Routes>
+                <Route>
+                  <Route
+                    path="/"
+                    element={!user ? <Home /> : <Navigate to="/" />}
+                  />{" "}
+                  <Route
+                    path="/login"
+                    element={!user ? <Login /> : <Navigate to="/dashboard" />}
+                  />
+                  <Route
+                    path="/signup"
+                    element={!user ? <Signup /> : <Navigate to="/dashboard" />}
+                  />
+                </Route>
+                <Route>
+                  <Route
+                    path="/dashboard"
+                    element={user ?
+                      <MyProSidebarProvider>
+                        <div>
+                          <Topbar className="h-100 w-100" />
+                          <Dashboard />
+                        </div>
+                      </MyProSidebarProvider> : <Navigate to="/login"/>
+                    }
+                  />
+                </Route>
+              </Routes>
+            </ThemeProvider>
+          </ColorModeContext.Provider>
+        </div>
+      </>
     </BrowserRouter>
   );
 }
