0fd36fc0f872bcf3587ad2b73741201d1636839a
AIT HELLAL Djaaffer <djaaffer.aithellal@gmail.com>
2023-01-02 13:34:45 +0100

    editing event card

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -1,10 +1,35 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Auth } from "../contexts/Auth";
-import EventCard from "./EventForm";
+import EventForm from "./EventForm";
 
-export default function Events() {
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+import { tokens } from "../theme";
+import { mockTransactions } from "../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import Header from "../components/Header";
+
+
+import StatBox from "../components/StatBox";
+import ProgressCircle from "../components/ProgressCircle";
+
+export default function EventCard() {
   const { user } = useContext(Auth);
   const [events, setEvents] = useState([]);
+
+  const theme = useTheme();
+  const smScreen = useMediaQuery(theme.breakpoints.up("sm"));
+  const colors = tokens(theme.palette.mode);
 
   useEffect(() => {
     if (!user) {
@@ -26,9 +51,9 @@ export default function Events() {
 
   return (
     <>
-      <div>
+      <div className="flex flex-row gap-1 w-full ">
         {events.map((event) => {
-          return <EventCard event={event} key={event._id} />;
+          return <EventForm event={event} key={event._id} />;
         })}
       </div>
     </>
