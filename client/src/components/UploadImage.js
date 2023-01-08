0087c258c9a48260504b220fa196585ea10ad3f4
AIT HELLAL Djaaffer <djaaffer.aithellal@gmail.com>
2023-01-08 14:21:19 +0100

    upload image to aws

--- client/src/components/UploadImage.js
+++ client/src/components/UploadImage.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import ImageUploading from 'react-images-uploading';
+import React, { useContext } from "react";
+import ImageUploading from "react-images-uploading";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import { Auth } from "../contexts/Auth";
 
 export function UploadImage() {
   const [images, setImages] = React.useState([]);
+  const [image, setImage] = React.useState([]);
   const maxNumber = 69;
+
+  const user = useContext(Auth);
 
   const onChange = (imageList, addUpdateIndex) => {
     // data for submit
-    console.log(imageList, addUpdateIndex);
+    console.log(addUpdateIndex);
     setImages(imageList);
+    setImage(imageList[0]);
+    console.log(image);
+  };
+
+  const handelSubmit = async () => {
+    if (!image) {
+      return null;
+    }
+    const newTitle = `${image.file.name
+      .replaceAll(" ", "")
+      .toLowerCase()}${uuidv4()}`;
+
+    const response = await fetch(`api/uploadURL/${newTitle}`, {
+      method: "GET",
+    });
+    const data = await response.json();
+    console.log(data);
+    const { put, get, key } = data;
+    try {
+      await fetch(put, {
+        method: "PUT",
+        body: JSON.stringify(image),
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+    // sendDAtaToDAtaBase(key);
   };
 
   return (
@@ -32,7 +65,7 @@ export function UploadImage() {
           // write your building UI
           <div className="upload__image-wrapper">
             <button
-              style={isDragging ? { color: 'red' } : undefined}
+              style={isDragging ? { color: "red" } : undefined}
               onClick={onImageUpload}
               {...dragProps}
             >
@@ -42,10 +75,11 @@ export function UploadImage() {
             <button onClick={onImageRemoveAll}>Remove all images</button>
             {imageList.map((image, index) => (
               <div key={index} className="image-item">
-                <img src={image['data_url']} alt="" width="100" />
+                <img src={image["data_url"]} alt="" width="100" />
                 <div className="image-item__btn-wrapper">
                   <button onClick={() => onImageUpdate(index)}>Update</button>
                   <button onClick={() => onImageRemove(index)}>Remove</button>
+                  <button onClick={() => handelSubmit()}>submit</button>
                 </div>
               </div>
             ))}
--- server/services/upload.js
+++ server/services/upload.js
@@ -19,6 +19,7 @@ const createS3Bucket = async () => {
 
 
 const getUploadPresignedUrl = async (filename, oldKey) => {
+  console.log("this is from services", filename)
   const key =
     oldKey ||
     crypto.randomBytes(24).toString("hex") + (filename ? "-" + filename : "");
